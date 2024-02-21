be259678edeff7beee972a986d8addab3a6a7631
hainiu <hainiu@example.com>
2024-02-21 21:20:19 +0800

    V1.0

--- com/hainiuxy/Demo.java
+++ com/hainiuxy/Demo.java
@@ -1,5 +1,10 @@
+package com.hainiuxy;
+
 public class Demo{
 	public static void main(String[] args){
+
 		System.out.println("Hello World");
+		System.out.println(1);
+		System.out.println(2);
 	}
 }
